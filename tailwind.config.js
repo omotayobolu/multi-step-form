25e2b06cfff4379b751f13fff1790fee241f7348
Boluwatife <omotayodaniel10@gmail.com>
2023-02-17 12:16:15 +0100

    starter files

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Ubuntu: ["Ubuntu", "sans-serif"],
+      },
+      colors: {
+        Marine_blue: "hsl(213, 96%, 18%)",
+        Purplish_blue: "hsl(243, 100%, 62%)",
+        Pastel_blue: "hsl(228, 100%, 84%)",
+        Light_blue: "hsl(206, 94%, 87%)",
+        Strawberry_red: "hsl(354, 84%, 57%)",
+        Cool_gray: "hsl(231, 11%, 63%)",
+        Light_gray: " hsl(229, 24%, 87%)",
+        Magnolia: "hsl(217, 100%, 97%)",
+        Alabaster: "hsl(231, 100%, 99%)",
+        White: "hsl(0, 0%, 100%)",
+      },
+      fontSize: {
+        // base: "16px",
+      },
+    },
+  },
+  plugins: [],
+};
